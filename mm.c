19491a4b8f7539bb4e49d504f0043690be5577fe
Manit Gandhi <yourgitemail@example.com>
2022-03-27 13:46:53 +0700

    Successfully use threads to load and reduce time to load matrix significantly

--- mm.c
+++ mm.c
@@ -4,10 +4,7 @@
 #include <math.h>
 #include <pthread.h>
 #include "mm.h"
-#define MAXBUFF 1000
 
-long row = SIZEX;	    //row
-long col = SIZEY;
 
 // Task 1: Flush the cache so that we can do our measurement :)
 void flush_all_caches()
@@ -15,7 +12,7 @@ void flush_all_caches()
     // Your code goes here
 }
 
-void load_matrix_A()
+void *load_matrix_A()
 {
     long i;
     huge_matrixA = malloc(sizeof(long)*(long)SIZEX*(long)SIZEY);
@@ -26,9 +23,10 @@ void load_matrix_A()
     {
         fscanf(fin1,"%ld", (huge_matrixA+i));
     }
+    return NULL;
 }
 
-void load_matrix_B()
+void *load_matrix_B()
 {
     long i;
     huge_matrixB = malloc(sizeof(long)*(long)SIZEX*(long)SIZEY);
@@ -39,9 +37,10 @@ void load_matrix_B()
     {
         fscanf(fin2,"%ld", (huge_matrixB+i));
     }
+    return NULL;
 }
 
-void set_matrix_C()
+void *set_matrix_C()
 {
     huge_matrixC = malloc(sizeof(long)*(long)SIZEX*(long)SIZEY);
     long i;
@@ -49,13 +48,20 @@ void set_matrix_C()
     {
         huge_matrixC[i] = 0;
     }
+    return NULL;
 }
 
 void load_matrix_base()
 {
-    load_matrix_A();
-    load_matrix_B();
-    set_matrix_C();
+    pthread_t thread;
+    pthread_create(&thread, NULL, load_matrix_A, NULL);
+    pthread_create(&thread, NULL, load_matrix_B, NULL);
+    pthread_create(&thread, NULL, set_matrix_C, NULL);
+    pthread_join(thread, NULL);
+
+//    load_matrix_A();
+//    load_matrix_B();
+//    set_matrix_C();
     // Load the input
     // Note: This is suboptimal because each of these loads can be done in parallel.
 
@@ -78,6 +84,11 @@ void multiply_base()
     printf("%ld\n",huge_matrixA[1]);
     printf("%ld\n",huge_matrixA[2]);
     printf("%ld\n",huge_matrixA[3]);
+    printf("%ld\n",huge_matrixB[0]);
+    printf("%ld\n",huge_matrixB[1]);
+    printf("%ld\n",huge_matrixB[2]);
+    printf("%ld\n",huge_matrixB[3]);
+
     // Your code here
     //
     // Implement your baseline matrix multiply here.
